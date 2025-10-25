2fd2f5ef37a477fc5b50b6495ce0251343c24b02
yra <ymz.ncnk@outlook.com>
2025-10-25 15:37:30 +0100

    Correct comments

--- client/delegate.go
+++ client/delegate.go
@@ -1,19 +1,3 @@
-// Package client provides client-side implementations for the delegate
-// abstraction of the cmd-stream-go library.
-//
-// It defines several Delegate types that implement the core.ClientDelegate
-// and core.ClientReconnectDelegate interfaces.
-//
-// Key delegates:
-//
-//   - Delegate: basic client delegate that receives ServerInfo.
-//   - KeepaliveDelegate: extends Delegate with a ping-pong mechanism to keep
-//     the connection alive when no Commands are pending.
-//   - ReconnectDelegate: extends Delegate with automatic reconnect logic
-//     when the connection to the server is lost.
-//
-// All delegates rely on a pluggable Transport for data exchange and support
-// configurable options such as send/receive deadlines.
 package client
 
 import (
--- transport.go
+++ transport.go
@@ -1,5 +1,3 @@
-// Package delegate provides client-server communication abstractions for
-// cmd-stream-go.
 package delegate
 
 import (
